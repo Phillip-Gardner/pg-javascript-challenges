da88b97a934e2d4f35754afb144d0df25d5ad00d
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-26 16:27:59 +0100

    first few tests

--- 01-challenge-one/functions.test.js
+++ 01-challenge-one/functions.test.js
@@ -1,5 +1,23 @@
 const functions = require('./functions');
 
-test('Adds 2+2 to equal 4', () => {
+test('Adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test('Adds 2 + 2 to not equal 5', () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+})
+
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull;
+})
+
+test('Should be falsey', () => {
+  expect(functions.checkValue(null)).toBeFalsey;
+})
+
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsey matches anything that an if statement treats as false
