88ec8bbe8f1b2827ef8c882eddf58e7c9664741a
Phillip Gardner <pbg.gardner@gmail.com>
2021-04-26 16:36:15 +0100

    added another test

--- 01-challenge-one/functions.test.js
+++ 01-challenge-one/functions.test.js
@@ -6,18 +6,33 @@ test('Adds 2 + 2 to equal 4', () => {
 
 test('Adds 2 + 2 to not equal 5', () => {
   expect(functions.add(2, 2)).not.toBe(5);
-})
-
-test('Should be null', () => {
-  expect(functions.isNull()).toBeNull;
-})
-
-test('Should be falsey', () => {
-  expect(functions.checkValue(null)).toBeFalsey;
-})
+});
 
 // toBeNull matches only null
 // toBeUndefined matches only undefined
 // toBeDefined is the opposite of toBeUndefined
+
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull;
+});
+
 // toBeTruthy matches anything that an if statement treats as true
 // toBeFalsey matches anything that an if statement treats as false
+
+test('Should be falsey', () => {
+  expect(functions.checkValue(null)).toBeFalsey;
+});
+
+test('Should be truthy', () => {
+  expect(functions.checkValue(2)).toBeTruthy;
+});
+
+// toEqual for objects and more complex data types
+
+test('User should be phillip gardner obj', () => {
+  expect(functions.createUser()).toEqual({
+    firstName: 'Phil',
+    lastName: 'Gardner'
+  });
+});
+
