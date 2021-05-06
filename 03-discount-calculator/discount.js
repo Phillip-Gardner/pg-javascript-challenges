c82a7f0a6cf1cfba00a19b20df2f807b11f5263b
Phillip Gardner <pbg.gardner@gmail.com>
2021-05-06 16:32:21 +0100

    added jest

--- 03-discount-calculator/discount.js
+++ 03-discount-calculator/discount.js
@@ -2,3 +2,27 @@ function sum(a, b) {
   return a + b;
 }
 module.exports = sum;
+
+
+
+checkout = () => {
+        let totalPrice = 0;
+        let loyaltyPointsEarned = 0;
+
+        this.products.forEach(product => {
+            let discount = 0;
+            if (product.code.startsWith("DIS_10")) {
+                discount = product.price * 0.1;
+                loyaltyPointsEarned += product.price / 10;
+            } else if (product.code.startsWith("DIS_15")) {
+                discount = product.price * 0.15;
+                loyaltyPointsEarned += product.price / 15;
+            } else {
+                loyaltyPointsEarned += product.price / 5;
+            }
+
+            totalPrice += product.price - discount;
+        });
+
+        return new Order(totalPrice, loyaltyPointsEarned);
+    };
